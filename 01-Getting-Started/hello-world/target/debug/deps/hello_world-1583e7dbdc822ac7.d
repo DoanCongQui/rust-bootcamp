ca17dd52155808a8132f0ff7fb04717c27cf07f2
DoanCongQui <congt4644@gmail.com>
2025-01-28 00:39:54 +0700

    remove tager

--- 01-Getting-Started/hello-world/target/debug/deps/hello_world-1583e7dbdc822ac7.d
+++
@@ -1,5 +0,0 @@
-/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/deps/hello_world-1583e7dbdc822ac7: src/main.rs
-
-/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/deps/hello_world-1583e7dbdc822ac7.d: src/main.rs
-
-src/main.rs:
--- 01-Getting-Started/hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/hello-world: /home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/src/main.rs
