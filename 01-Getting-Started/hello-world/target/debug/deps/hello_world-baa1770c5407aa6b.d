f3733048a56ca23be6630effc13533c5e29a2417
DoanCongQui <congt4644@gmail.com>
2025-01-28 00:38:55 +0700

    Getting Start

---
+++ 01-Getting-Started/hello-world/target/debug/deps/hello_world-baa1770c5407aa6b.d
@@ -0,0 +1,5 @@
+/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/deps/libhello_world-baa1770c5407aa6b.rmeta: src/main.rs
+
+/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/deps/hello_world-baa1770c5407aa6b.d: src/main.rs
+
+src/main.rs:
---
+++ 01-Getting-Started/hello-world/target/debug/deps/hello_world-d2851f55aa074f03.d
@@ -0,0 +1,5 @@
+/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/deps/libhello_world-d2851f55aa074f03.rmeta: src/main.rs
+
+/home/dq/.code/rust-bootcamp/01-Getting-Started/hello-world/target/debug/deps/hello_world-d2851f55aa074f03.d: src/main.rs
+
+src/main.rs:
