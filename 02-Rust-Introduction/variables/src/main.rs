cca155c7241a2c883c970df47691c2ae33be53ec
DoanCongQui <congt4644@gmail.com>
2025-01-30 00:03:23 +0700

    Update

--- 02-Rust-Introduction/variables/src/main.rs
+++ 02-Rust-Introduction/variables/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+
+    // Dinh nghia gia tri x = 5
+    // bien let la bat bien immurtable
+    let x = 5;
+
+    // In ra man hin gia tri bien do 
+    println!("{}", x);
 }
