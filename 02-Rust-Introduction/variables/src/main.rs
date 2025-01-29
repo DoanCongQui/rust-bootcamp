08bd9ef5c71a46e06d94f42d78cd3acecef9444a
DoanCongQui <congt4644@gmail.com>
2025-01-30 00:05:46 +0700

    Update

--- 02-Rust-Introduction/variables/src/main.rs
+++ 02-Rust-Introduction/variables/src/main.rs
@@ -3,8 +3,10 @@ fn main() {
 
     // Dinh nghia gia tri x = 5
     // bien let la bat bien immurtable
-    let x = 5;
-
+    let mut x = 5;
+    
+    // muon gian vao cho 1 bien them [mut] vao bien 
+    x = x+10;
     // In ra man hin gia tri bien do 
     println!("{}", x);
 }
